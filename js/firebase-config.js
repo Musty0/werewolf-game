981514281322b30801cc86b95fda7e18991a1da7
Musty0 <mustafa.musa1234@gmail.com>
2026-06-23 07:27:41 +0100

    update

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,10 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyBfubqkhkUvvf_-lnIz6va7YUI-0Fhyna0",
   authDomain: "werewolf-a9450.firebaseapp.com",
-  databaseURL: "https://werewolf-a9450-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL:
+    "https://werewolf-a9450-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "werewolf-a9450",
   storageBucket: "werewolf-a9450.firebasestorage.app",
   messagingSenderId: "364810568417",
-  appId: "1:364810568417:web:e80c371bd3e4378acaeda0"
+  appId: "1:364810568417:web:e80c371bd3e4378acaeda0",
 };
